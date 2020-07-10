84cd26f684113db8cfd79f639aeb8426b6d8eaf4
PickBas <sayed.kirill@gmail.com>
2020-07-10 11:50:29 +0300

    Initial

---
+++ src/test/java/com/therearenotasksforus/videohostingapi/VideoHostingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therearenotasksforus.videohostingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoHostingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
