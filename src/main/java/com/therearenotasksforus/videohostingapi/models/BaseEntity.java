ca63ea1346f4ba84acbd471297d82a92b6de395e
PickBas <sayed.kirill@gmail.com>
2020-07-12 13:07:58 +0300

    Added basic models

---
+++ src/main/java/com/therearenotasksforus/videohostingapi/models/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.therearenotasksforus.videohostingapi.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created")
+    private Date created;
+
+    @LastModifiedDate
+    @Column(name = "updated")
+    private Date updated;
+
+    protected Long getId() {
+        return id;
+    }
+}
