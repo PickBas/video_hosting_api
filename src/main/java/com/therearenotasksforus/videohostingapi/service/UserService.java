15fce84ee2ed71b0965a5ecc3ddce824b40c9071
Kirill Sayed <sayed.kirill@gmail.com>
2020-11-21 14:16:58 +0300

    Refactored user service

--- src/main/java/com/therearenotasksforus/videohostingapi/service/UserService.java
+++ src/main/java/com/therearenotasksforus/videohostingapi/service/UserService.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public interface UserService {
     void register(User user);
+    void checkIfUserExists(User user);
 
     void updatePassword(User user, String password);
-
     void updateNames(User user, UpdateUserDto updateUserDto) throws ValidationException;
 
     List<User> getAll();
