40db131feeb074a02e55e750f014d48be639fc9c
Kirill Sayed <sayed.kirill@gmail.com>
2022-01-21 12:33:05 +0300

    Refactored UserTests

--- src/test/java/com/therearenotasksforus/videohostingapi/UserTests.java
+++ src/test/java/com/therearenotasksforus/videohostingapi/UserTests.java
@@ -1,5 +1,6 @@
 package com.therearenotasksforus.videohostingapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -17,8 +18,6 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = VideoHostingApiApplication.class,
@@ -50,12 +49,11 @@ class UserTests {
         String uri = "/api/users";
         MvcResult mvcResult = TestMethods.getRequest(mvc, uri, userToken);
         ArrayList<Map<String, Object>> responseBodyArray = TestMethods.mapFromJsonList(mvcResult);
-
-        assertEquals(200, mvcResult.getResponse().getStatus());
-        assertNotEquals(0, responseBodyArray.size());
-        assertNotEquals(null, responseBodyArray.get(0).get("username"));
-        assertEquals(responseBodyArray.get(0).get("id"), responseBodyArray.get(0).get("profile"));
-        assertNotEquals(null, responseBodyArray.get(0).get("email"));
+        Assertions.assertEquals(200, mvcResult.getResponse().getStatus());
+        Assertions.assertNotEquals(0, responseBodyArray.size());
+        Assertions.assertNotEquals(null, responseBodyArray.get(0).get("username"));
+        Assertions.assertEquals(responseBodyArray.get(0).get("id"), responseBodyArray.get(0).get("profile"));
+        Assertions.assertNotEquals(null, responseBodyArray.get(0).get("email"));
     }
 
     @Test
@@ -63,11 +61,10 @@ class UserTests {
         String uri = "/api/user/id/1";
         MvcResult mvcResult = TestMethods.getRequest(mvc, uri, userToken);
         Map<String, Object> responseBody = TestMethods.mapFromJson(mvcResult);
-
-        assertEquals(200, mvcResult.getResponse().getStatus());
-        assertNotEquals(null, responseBody.get("username"));
-        assertEquals(responseBody.get("id"), responseBody.get("profile"));
-        assertNotEquals(null, responseBody.get("email"));
+        Assertions.assertEquals(200, mvcResult.getResponse().getStatus());
+        Assertions.assertNotEquals(null, responseBody.get("username"));
+        Assertions.assertEquals(responseBody.get("id"), responseBody.get("profile"));
+        Assertions.assertNotEquals(null, responseBody.get("email"));
     }
 
     @Test
@@ -75,36 +72,30 @@ class UserTests {
         String uri = "/api/user";
         MvcResult mvcResult = TestMethods.getRequest(mvc, uri, userToken);
         Map<String, Object> responseBody = TestMethods.mapFromJson(mvcResult);
-
-        assertEquals(200, mvcResult.getResponse().getStatus());
-        assertNotEquals(null, responseBody.get("username"));
-        assertEquals(responseBody.get("id"), responseBody.get("profile"));
-        assertNotEquals(null, responseBody.get("email"));
+        Assertions.assertEquals(200, mvcResult.getResponse().getStatus());
+        Assertions.assertNotEquals(null, responseBody.get("username"));
+        Assertions.assertEquals(responseBody.get("id"), responseBody.get("profile"));
+        Assertions.assertNotEquals(null, responseBody.get("email"));
     }
 
     @Test
     public void userUpdate() throws Exception {
         String uri = "/api/user/update";
-
         Map<String, String> requestBody = new HashMap<>();
         requestBody.put("firstName", "updated_first_name");
         requestBody.put("lastName", "updated_last_name");
-
         String jsonBody = TestMethods.mapToJson(requestBody);
         MvcResult mvcResult = TestMethods.postRequest(mvc, uri, userToken, jsonBody);
         Map <String, Object> responseBody = TestMethods.mapFromJson(mvcResult);
-
-        assertEquals(200, mvcResult.getResponse().getStatus());
-        assertEquals("updated_first_name", responseBody.get("firstName"));
-        assertEquals("updated_last_name", responseBody.get("lastName"));
-
+        Assertions.assertEquals(200, mvcResult.getResponse().getStatus());
+        Assertions.assertEquals("updated_first_name", responseBody.get("firstName"));
+        Assertions.assertEquals("updated_last_name", responseBody.get("lastName"));
         uri = "/api/user";
         mvcResult = TestMethods.getRequest(mvc, uri, userToken);
         responseBody = TestMethods.mapFromJson(mvcResult);
-
-        assertEquals(200, mvcResult.getResponse().getStatus());
-        assertEquals("updated_first_name", responseBody.get("firstName"));
-        assertEquals("updated_last_name", responseBody.get("lastName"));
+        Assertions.assertEquals(200, mvcResult.getResponse().getStatus());
+        Assertions.assertEquals("updated_first_name", responseBody.get("firstName"));
+        Assertions.assertEquals("updated_last_name", responseBody.get("lastName"));
     }
 
     @Test
@@ -116,13 +107,10 @@ class UserTests {
                 Map.entry("updated_password", update_password),
                 Map.entry("old_password", old_password)
         ));
-
         MvcResult mvcResult = TestMethods.postRequest(mvc, uri, userToken, jsonBody);
         Map<String, Object> response = TestMethods.mapFromJson(mvcResult);
-
-        assertEquals(200, mvcResult.getResponse().getStatus());
-        assertTrue(new BCryptPasswordEncoder().matches(update_password, (String) response.get("password")));
-
+        Assertions.assertEquals(200, mvcResult.getResponse().getStatus());
+        Assertions.assertTrue(new BCryptPasswordEncoder().matches(update_password, (String) response.get("password")));
         TestMethods.postRequest(mvc, uri, userToken, TestMethods.mapToJson(Map.ofEntries(
                 Map.entry("updated_password", old_password),
                 Map.entry("old_password", update_password)
