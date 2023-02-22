b0785c7ce21dfde95e6d7166ad84a86a325cfa1a
Kirill Saied <sayed.kirill@gmail.com>
2023-02-22 17:30:38 +0300

    Added javadoc for UserService

--- src/main/java/com/saied/videohostingapi/service/UserService.java
+++ src/main/java/com/saied/videohostingapi/service/UserService.java
@@ -1,20 +1,71 @@
 package com.saied.videohostingapi.service;
 
 import com.saied.videohostingapi.dto.user.UpdateUserDto;
-import com.saied.videohostingapi.models.Profile;
 import com.saied.videohostingapi.models.User;
 
 import jakarta.validation.ValidationException;
 import java.util.List;
 
 public interface UserService {
+
+    /**
+     * Registering user
+     * @param user User entity
+     */
     void register(User user);
-    void checkIfUserExists(User user);
-    void updatePassword(User user, String password);
-    void updateNames(User user, UpdateUserDto updateUserDto) throws ValidationException;
-    List<User> getUsersPaginated(int numberPerPage, int page);
+
+    /**
+     * Checking user's existence
+     * @param userId User id
+     */
+    boolean checkIfUserExists(Long userId);
+
+    /**
+     * Updating user's password
+     * @param userId User id
+     * @param password New passowrd
+     */
+    void updatePassword(Long userId, String password);
+
+    /**
+     * Updating user data
+     * @param userId User id
+     * @param updateUserDto New user data
+     * @throws ValidationException If provided data is invalid
+     */
+    void updateNames(Long userId, UpdateUserDto updateUserDto) throws ValidationException;
+
+    /**
+     * Listing users with pagination
+     * @param page Number of the page
+     * @return List of users
+     */
+    List<User> getUsersPaginated(int page);
+
+    /**
+     * Finding user by id
+     * @param id User id
+     * @return User entity
+     */
     User findById(Long id);
-    User findByProfile(Profile profile);
+
+    /**
+     * Finding user by profile
+     * @param profileId ProfileId
+     * @return User entity
+     */
+    User findByProfile(Long profileId);
+
+    /**
+     * Finding user by username
+     * @param username Username
+     * @return User entity
+     */
     User findByUsername(String username);
+
+    /**
+     * Deleting user
+     * @param id User id
+     */
     void delete(Long id);
 }
